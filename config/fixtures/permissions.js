a46839c36dc9049a6f8cf3c9f16ce91388fe7dda
acm1pt-colorado <luisadanjimenez@gmail.com>
2016-05-02 03:43:25 +0200

    añadir permisos update

--- config/fixtures/permissions.js
+++ config/fixtures/permissions.js
@@ -5,11 +5,26 @@ var Promise = require('bluebird');
 
 exports.create = function () {
 
+
+		var actions = ['read', 'create', 'delete', 'update'];
+		var modelosAlumno = ['Termino', 'Definicion', 'Materia'];
+		var permisosAlumno = [];
+
+		actions.forEach(function(action){
+			modelosAlumno.forEach(function(modeloAlumno){
+				permiso = {role: 'Alumno', model: modeloAlumno, action: action};
+				if (action == 'delete') {permiso.relation = 'owner'};
+				permisosAlumno.push(permiso);
+			})
+		})
+		//sails.log.verbose(permisosProfesor);
+
 	  return Promise.all([
 	    Role.findOrCreate({ name: 'profesor' }, { name: 'profesor' }),
 	    Role.findOrCreate({ name: 'alumno' }, { name: 'alumno' })
 	  ]).then(function(role){
 	  		return Promise.all([
+	  			PermissionService.grant(permisosAlumno),
 			    PermissionService.grant({ role: 'profesor', model: 'Termino', action: 'read'}), 
 			    PermissionService.grant({ role: 'profesor', model: 'Termino', action: 'create'}), 
 			    PermissionService.grant({ role: 'profesor', model: 'Termino', action: 'delete', relation: 'owner'}), 
@@ -18,12 +33,7 @@ exports.create = function () {
 			    PermissionService.grant({ role: 'profesor', model: 'Definicion', action: 'create'}), 
 			    PermissionService.grant({ role: 'profesor', model: 'Definicion', action: 'delete', relation: 'owner'}), 
 			    PermissionService.grant({ role: 'profesor', model: 'Definicion', action: 'update'}), 
-			    PermissionService.grant({ role: 'alumno', model: 'Termino', action: 'read'}), 
-			    PermissionService.grant({ role: 'alumno', model: 'Termino', action: 'create'}), 
-			    PermissionService.grant({ role: 'alumno', model: 'Termino', action: 'update'}), 
-			    PermissionService.grant({ role: 'alumno', model: 'Definicion', action: 'read'}),
-			    PermissionService.grant({ role: 'alumno', model: 'Definicion', action: 'create'}),
-			    PermissionService.grant({ role: 'alumno', model: 'Definicion', action: 'update'})
+			   
 		  ])
 	  })
 };
